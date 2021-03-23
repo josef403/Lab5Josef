d418e7eccc67ae2bd55db0175b8dd0926a8625e2
Josef Omari <josef401@hotmail.com>
2021-03-23 20:43:23 +0100

    Add project files.

---
+++ Lab5Josef/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Lab5Josef
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(668, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(784, 374);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.WordWrap = false;
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExtract.Location = new System.Drawing.Point(686, 12);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(110, 20);
+            this.buttonExtract.TabIndex = 2;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = true;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Found 0 images";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(671, 422);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(110, 20);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save images";
+            this.buttonSave.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Visible = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(350, 150);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ImageScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ Lab5Josef/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5Josef
+{
+    public partial class Form1 : Form
+    {
+        private string _folderpath;
+        private string _htmlcode;
+        private string[] _imageArray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Gör så att du kan trycka enter istället för att klicka på knappen
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //Kollar så att textboxen inte är tom när man extractar
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    MessageBox.Show("Enter a url.");
+                    return;
+                }
+                buttonExtract_Click(this, new EventArgs());
+            }
+        }
+        private async void buttonExtract_Click(object sender, EventArgs e)
+        {
+            //Kollar så att textboxen inte är tom när man extractar
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Enter a url.");
+                return;
+            }
+            textBox2.Clear();
+
+            await ImageChecker(textBox1.Text);
+            string pattern = @"(?<=src=""\/image)(.*?)(?="")";
+            Regex rgx = new Regex(pattern);
+            var results = rgx.Matches(_htmlcode);
+            //Skapar en bild array med antalet matchningar den hittar
+            _imageArray = new string[results.Count];
+            int countedImages = 0;
+
+            //Lägger till alla urls för bilderna i textboxen och spara de i en array
+            foreach (Match match in results)
+            {
+                textBox2.Text += "https://gp.se/image" + match + Environment.NewLine;
+                _imageArray[countedImages] = "https://gp.se/image" + match.Value;
+                countedImages++;
+            }
+
+            //Räknar hur många bilder den hittade hos gp.se
+            label1.Text = $"Found {countedImages.ToString()} images.";
+            //Gör Save knappen bara synlig ifall man klickat på extract
+            buttonSave.Visible = true;
+        }
+
+        public async Task ImageChecker(string url)
+        {
+            /*gör det simplare att skriva in en url
+            genom att lägga till http:// om det inte finns */
+
+            string urlStart = @"https://";
+            Regex rgx = new Regex(urlStart);
+            var urlcheck = rgx.Matches(url);
+
+            if (urlcheck.Count == 0)
+            {
+                url = @"https://" + url;
+            }
+
+            var client = new HttpClient();
+
+            Task<string> website = client.GetStringAsync(url);
+            await website;
+            _htmlcode = website.Result;
+        }
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                _folderpath = fbd.SelectedPath;
+                await DownloadImages();
+            }
+        }
+
+        public async Task DownloadImages()
+        {
+            var httpClient = new HttpClient();
+
+            for (int i = 0; i < _imageArray.Length; i++)
+            {
+                byte[] imageBytes = await httpClient.GetByteArrayAsync(_imageArray[i]);
+                string format = string.Empty;
+                switch (_imageArray[i])
+                {
+                    case string a when a.Contains(".jpg"):
+                        format = ".jpg";
+                        break;
+                    case string b when b.Contains(".png"):
+                        format = ".png";
+                        break;
+                    case string c when c.Contains(".gif"):
+                        format = ".gif";
+                        break;
+                    case string d when d.Contains(".bmp"):
+                        format = ".bmp";
+                        break;
+                    case string e when e.Contains(".jpeg"):
+                        format = ".jpeg";
+                        break;
+                }
+
+                //Skapar bilden i  din valda map.
+                File.WriteAllBytes(_folderpath + $"/image{i + 1}{format}", imageBytes);
+                //Räknar antalet nedladdningar asynchronous.
+                label1.Text = $"Downloaded {i + 1} out of {_imageArray.Length} images.";
+            }
+        }
+    }
+}
